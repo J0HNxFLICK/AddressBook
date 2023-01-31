c011fd5ada222573b4f709e1822b2a72010bea19
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2023-01-31 21:33:01 +0530

    [Arun] Able to edit contact based on first name

--- AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBook.cs
@@ -12,6 +12,45 @@ namespace AddressBookSystem
     {
         public static List<Contact> contacts = new List<Contact>();
 
+        public static void Prompt()
+        {
+            Console.WriteLine("Do you want to perform other operations y/n ?");
+            char promptChoice = Convert.ToChar(Console.ReadLine());
+
+            if(promptChoice == 'y')
+            {
+                Console.WriteLine("\n1 - Add Contact\n2 - Edit Existing Contact\n");
+                int promptUserChoice = Convert.ToInt32(Console.ReadLine());
+
+                switch(promptUserChoice)
+                {
+                    case 1:
+                        AddContact();
+                        break;
+
+                    case 2:
+                        if (contacts.Count != 0)
+                        {
+                            EditContact();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Address Book is empty.");
+                            Prompt();
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Choose appropriate option.");
+                        Prompt();
+                        break;
+                }
+            }
+
+            else
+                Console.WriteLine("Thank you for using Address Book program");
+        }
+
         public static void Display()
         {
             foreach (Contact contact in contacts)
@@ -54,8 +93,6 @@ namespace AddressBookSystem
                 int zipcodeNumb = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("");
 
-                //list.Add(new Customer { Id = 10, Name = "test" });
-
                 contacts.Add(new Contact { firstName = first, lastName= last, email = emailId, phone = phoneNumber, city = cityName, state = stateName, zipcode = zipcodeNumb } );
 
                 Console.WriteLine("Contact Successfully created !\n");
@@ -69,7 +106,170 @@ namespace AddressBookSystem
 
             Console.WriteLine("Available contacts ...\n");
             AddressBook.Display();
+
+            Prompt();
             
+        }
+
+        public static void EditContact() // UC3 Editing a contact based on first name
+        {
+            Console.Write("Enter First Name of the contact you want to edit : \n");
+            string nameInput = Console.ReadLine();
+
+            foreach (Contact contact in contacts)
+            {
+                if(contact.firstName == nameInput)
+                {
+                    char choice;
+                    
+                        Console.WriteLine("First Name : " + contact.firstName);
+                        Console.WriteLine("Last Name : " + contact.lastName);
+                        Console.WriteLine("Email ID : " + contact.email);
+                        Console.WriteLine("Phone Number : " + contact.phone);
+                        Console.WriteLine("City : " + contact.city);
+                        Console.WriteLine("State : " + contact.state);
+                        Console.WriteLine("Zipcode : " + contact.zipcode);
+
+                    do
+                    {
+
+                        Console.WriteLine("Which field you want to edit ? Choose a number.");
+                        Console.WriteLine("\n1 - First Name\n2 - Last Name\n3 - Email ID\n4 - Phone Number\n5 - City\n6 - State\n7 - Zipcode\n8 - All fields at once\n");
+                        int action = Convert.ToInt32(Console.ReadLine());
+
+                        switch (action)
+                        {
+                            case 1:
+
+                                Console.Write("Enter new First Name : ");
+                                string newFirstName = Console.ReadLine();
+                                Console.WriteLine("First name is now changed from {0} to {1}", contact.firstName, newFirstName);
+
+                                contact.firstName = newFirstName;
+                                break;
+
+                            case 2:
+
+                                Console.Write("Enter new Last Name : ");
+                                string newLastName = Console.ReadLine();
+                                Console.WriteLine("Last name is now changed from {0} to {1}", contact.lastName, newLastName);
+
+                                contact.lastName = newLastName;
+                                break;
+
+                            case 3:
+
+                                Console.Write("Enter new Email ID : ");
+                                string newEmailAddress = Console.ReadLine();
+                                Console.WriteLine("Email ID is now changed from {0} to {1}", contact.email, newEmailAddress);
+
+                                contact.email = newEmailAddress;
+                                break;
+
+                            case 4:
+
+                                Console.Write("Enter new Phone Number : ");
+                                long newPhoneNumber = Convert.ToInt64(Console.ReadLine());
+                                Console.WriteLine("Phone Number is now changed from {0} to {1}", contact.phone, newPhoneNumber);
+
+                                contact.phone = newPhoneNumber;
+                                break;
+
+                            case 5:
+
+                                Console.Write("Enter new City : ");
+                                string newCity = Console.ReadLine();
+                                Console.WriteLine("City is now changed from {0} to {1}", contact.city, newCity);
+
+                                contact.city = newCity;
+                                break;
+
+                            case 6:
+
+                                Console.Write("Enter new State : ");
+                                string newState = Console.ReadLine();
+                                Console.WriteLine("State is now changed from {0} to {1}", contact.state, newState);
+
+                                contact.state = newState;
+                                break;
+
+                            case 7:
+
+                                Console.Write("Enter new Zipcode : ");
+                                int newZipcode = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine("Zipcode is now changed from {0} to {1}", contact.zipcode, newZipcode);
+
+                                contact.zipcode = newZipcode;
+                                break;
+
+                            case 8:
+
+                                Console.Write("Enter First Name : ");
+                                string newfirst = Console.ReadLine();
+                                Console.WriteLine("");
+
+                                Console.Write("Enter Last Name : ");
+                                string newlast = Console.ReadLine();
+                                Console.WriteLine("");
+
+                                Console.Write("Enter email ID : ");
+                                string newEmailId = Console.ReadLine();
+                                Console.WriteLine("");
+
+                                Console.Write("Enter Phone Number : ");
+                                long newPhone = Convert.ToInt64(Console.ReadLine());
+                                Console.WriteLine("");
+
+                                Console.Write("Enter City : ");
+                                string newCityName = Console.ReadLine();
+                                Console.WriteLine("");
+
+                                Console.Write("Enter State : ");
+                                string newStateName = Console.ReadLine();
+                                Console.WriteLine("");
+
+                                Console.Write("Enter zipcode : ");
+                                int newZipcodeNumb = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine("");
+
+                                Console.WriteLine("Old contact : {0} {1} {2} {3} {4} {5} {6}\n", contact.firstName, contact.lastName, contact.email, contact.phone.ToString(), contact.city, contact.state, contact.zipcode);
+
+                                Console.WriteLine("New contact : {0} {1} {2} {3} {4} {5} {6}\n", newfirst, newlast, newEmailId, newPhone, newCityName, newStateName, newZipcodeNumb);
+
+                                contact.firstName = newfirst;
+                                contact.lastName = newlast;
+                                contact.email = newEmailId; 
+                                contact.phone = newPhone;
+                                contact.city = newCityName;
+                                contact.state = newStateName;
+                                contact.zipcode = newZipcodeNumb;
+
+                                Console.WriteLine("Contacted updated successfully !");
+                                break;
+
+                            default:
+
+                                Console.WriteLine("Choose appropriate choice.");
+                                break;
+                        }
+
+                        Console.WriteLine("First Name : " + contact.firstName);
+                        Console.WriteLine("Last Name : " + contact.lastName);
+                        Console.WriteLine("Email ID : " + contact.email);
+                        Console.WriteLine("Phone Number : " + contact.phone);
+                        Console.WriteLine("City : " + contact.city);
+                        Console.WriteLine("State : " + contact.state);
+                        Console.WriteLine("Zipcode : " + contact.zipcode);
+
+                        Console.WriteLine("Do you want to edit the same contact again y/n ?");
+                        choice = Convert.ToChar(Console.ReadLine());
+
+                    } while (choice == 'y');
+
+                    Prompt();
+
+                }
+            }
         }
     }
 }
