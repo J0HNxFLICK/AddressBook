70b2a07a34beb813368218cf5efbd9a2a3ec8e6e
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2023-01-31 18:55:53 +0530

    [Arun] Added code that can take single contact

---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace AddressBookSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book System Program");
+
+            //UC1 Creating contact in address book.
+
+            Console.WriteLine("Please Enter Contact Details\n");
+            Console.Write("\nFirst Name : ");
+            String firstName = Console.ReadLine();
+            Console.Write("\nLast Name : ");
+            String lastName = Console.ReadLine();
+            Console.Write("\nPhone Number : ");
+            int phoneNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEmail : ");
+            String email = Console.ReadLine(); ;
+            Console.Write("\nAddress : ");
+            String address = Console.ReadLine();
+            Console.Write("\nCity : ");
+            String city = Console.ReadLine();
+            Console.Write("\nState : ");
+            String state = Console.ReadLine();
+            Console.Write("\nZip : ");
+            int zipCode = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Added contact.\n\n" + firstName + "  " + lastName + "  " + phoneNumber + "  " + email + "  " + address + "  " + city + "  " + state + "  " + zipCode);
+        }
+    }
+}
